5a703f7e74373808a554c7f9292bdf32ad43cc4d
zhaoyaxi <zhaoyaxi@yaoking.cn>
2020-05-20 14:03:56 +0800

    我的第一个版本

---
+++ repo/test/a.php
@@ -0,0 +1,15 @@
+<?php
+namespace myrepo;
+class a{
+    public function __construct()
+    {
+    }
+
+    public function pp(){
+		echo "hello2222 xx";
+	}
+	public static function dp(){
+	    echo "333333333";
+    }
+
+}
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+include "./vendor/autoload.php";
+use myrepo\a;
+
+$a = new a();
+$a->pp();
+
+a::dp();
